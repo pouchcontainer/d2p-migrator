9a8b24b826476e6eeb1f82945bd907bd462a36ad
Michael Wan <zirenwan@gmail.com>
2018-11-04 04:41:04 -0500

    refactor: fix Makefile and add version info
    Signed-off-by: Michael Wan <zirenwan@gmail.com>

---
+++ version/version.go
@@ -0,0 +1,13 @@
+package version
+
+// Version package values is auto-generated, the following values will be overwrited at build time.
+var (
+	// Version represents the version of pouchd.
+	Version = "1.0.0"
+
+	// BuildTime is the time when pouch binary is built
+	BuildTime = "unknown"
+
+	// GitCommit is the commit id to build Pouch
+	GitCommit = "unknown"
+)
